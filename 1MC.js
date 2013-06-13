8c6ff9f116471d4f68f3587bde3b53204429c99d
unknown <jbell-masterson@KFKCWKS00302935.kauffman.org>
2013-06-13 15:59:40 -0500

    second commit

---
+++ 1MC.js
@@ -0,0 +1,23 @@
+var oMC = {
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "properties": { "City": "Denver ", "State": "CO", "lat": "39.737567", "lon": "-104.9847179" }, "geometry": { "type": "Point", "coordinates": [ -104.9847179, 39.737567 ] } }
+,
+{ "type": "Feature", "properties": { "City": "Cedar Rapids", "State": "IA", "lat": "41.9778795", "lon": "-91.6656232" }, "geometry": { "type": "Point", "coordinates": [ -91.6656232, 41.9778795 ] } }
+,
+{ "type": "Feature", "properties": { "City": "West Des Moines", "State": "IA", "lat": "41.5772115", "lon": "-93.711332" }, "geometry": { "type": "Point", "coordinates": [ -93.711332, 41.5772115 ] } }
+,
+{ "type": "Feature", "properties": { "City": "Kansas City", "State": "MO", "lat": "39.0997265", "lon": "-94.5785667" }, "geometry": { "type": "Point", "coordinates": [ -94.5785667, 39.0997265 ] } }
+,
+{ "type": "Feature", "properties": { "City": "St. Louis", "State": "MO", "lat": "38.6270025", "lon": "-90.1994042" }, "geometry": { "type": "Point", "coordinates": [ -90.1994042, 38.6270025 ] } }
+,
+{ "type": "Feature", "properties": { "City": "Chapel Hill", "State": "NC", "lat": "35.9131996", "lon": "-79.0558445" }, "geometry": { "type": "Point", "coordinates": [ -79.0558445, 35.9131996 ] } }
+,
+{ "type": "Feature", "properties": { "City": "Reno", "State": "NV", "lat": "39.5296329", "lon": "-119.8138027" }, "geometry": { "type": "Point", "coordinates": [ -119.8138027, 39.5296329 ] } }
+,
+{ "type": "Feature", "properties": { "City": "Chattanooga", "State": "TN", "lat": "35.0456297", "lon": "-85.3096801" }, "geometry": { "type": "Point", "coordinates": [ -85.3096801, 35.0456297 ] } }
+,
+{ "type": "Feature", "properties": { "City": "Houston", "State": "TX", "lat": "29.7601927", "lon": "-95.3693896" }, "geometry": { "type": "Point", "coordinates": [ -95.3693896, 29.7601927 ] } }
+
+]
+}
