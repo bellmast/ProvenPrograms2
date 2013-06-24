0e0376381779d59dcdbf1e8f167b355044d521fe
bellmast <jbm444@gmail.com>
2013-06-24 12:56:17 -0500

    Update Leaflet_Script3.js

--- Leaflet_Script3.js
+++ Leaflet_Script3.js
@@ -16,12 +16,31 @@ myColor = "orange"
 var MC = L.geoJson(oMC, {onEachFeature: function(feature, layer) { return layer.bindPopup(feature.properties.City)}, pointToLayer: pointToLayer})
 
 
-var overLays = {"Source Link": SL, "Maker Faire": MF, "Startup Weekend": SW, "Ice House": IH, "FastTrac": FT, "1 Million Cups": MC}
+
+var overLays = {
+"<div style='border: 1px solid black; background-color: yellow; opacity: 0.8; border-radius: 50px; width: 15px; height: 15px;'></div><div>Source Link</div>"
+:
+SL,
+"<div style='border: 1px solid black; background-color: purple; opacity: 0.8; border-radius: 50px; width: 15px; height: 15px;'></div><div>Maker Faire</div>"
+:
+MF,
+"<div style='border: 1px solid black; background-color: green; opacity: 0.8; border-radius: 50px; width: 15px; height: 15px;'></div><div>Startup Weekend</div>"
+:
+SW,
+"<div style='border: 1px solid black; background-color: white; opacity: 0.8; border-radius: 50px; width: 15px; height: 15px;'></div><div>Ice House</div>"
+:
+IH,
+"<div style='border: 1px solid black; background-color: blue; opacity: 0.8; border-radius: 50px; width: 15px; height: 15px;'></div><div>FastTrac</div>"
+:
+FT,
+"<div style='border: 1px solid black; background-color: orange; opacity: 0.8; border-radius: 50px; width: 15px; height: 15px;'></div><div>1 Million Cups</div>"
+:
+MC}
 
 function runProgram() {
 	map = L.map('map', {layers: [SL, MF, SW, IH, FT, MC]}).setView([39.0997, -94.5783], 5);
 	L.tileLayer('http://{s}.tile.cloudmade.com/801652580bcf4a2fbccf30c3b731b34c/39904/256/{z}/{x}/{y}.png', {
-    		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+    		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery ï¿½ <a href="http://cloudmade.com">CloudMade</a>',
     		maxZoom: 18
 	}).addTo(map);
 	L.control.layers(null, overLays).addTo(map);
