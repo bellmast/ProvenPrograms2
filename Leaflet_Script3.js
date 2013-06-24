5eaee30c083a5d5ca6c806fae44d17cf4388bed0
bellmast <jbm444@gmail.com>
2013-06-24 16:23:16 -0500

    Update Leaflet_Script3.js

--- Leaflet_Script3.js
+++ Leaflet_Script3.js
@@ -18,22 +18,22 @@ var MC = L.geoJson(oMC, {onEachFeature: function(feature, layer) { return layer.
 
 
 var overLays = {
-"<div class='event-type'><div style='border: 1px solid black; background-color: yellow; opacity: 0.8; border-radius: 50%; width: 13px; height: 13px;margin-right:5px;float:left;'></div><span>Source Link</span></div>"
+"<div><div style='border: 1px solid black; background-color: yellow; opacity: 1; border-radius: 50%; width: 13px; height: 13px;float:left; margin-top:-16px; margin-left: 20px'><span style='font-size: 12px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSource&nbspLink</span></div>"
 :
 SL,
-"<div class='event-type'><div style='border: 1px solid black; background-color: purple; opacity: 0.8; border-radius: 50%; width: 13px; height: 13px;margin-right:5px;float:left;'></div><span>Maker Faire</span></div>"
+"<div><div style='border: 1px solid black; background-color: purple; opacity: 1; border-radius: 50%; width: 13px; height: 13px;float:left; margin-top:-16px; margin-left: 20px'><span style='font-size: 12px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspMaker&nbspFaire</span></div>"
 :
 MF,
-"<div class='event-type'><div style='border: 1px solid black; background-color: green; opacity: 0.8; border-radius: 50%; width: 13px; height: 13px;margin-right:5px;float:left;'></div><span>Startup Weekend</span></div>"
+"<div><div style='border: 1px solid black; background-color: green; opacity: 1; border-radius: 50%; width: 13px; height: 13px;float:left; margin-top:-16px; margin-left: 20px'><span style='font-size: 12px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspStartup&nbspWeekend</span></div>"
 :
 SW,
-"<div class='event-type'><div style='border: 1px solid black; background-color: white; opacity: 0.8; border-radius: 50%; width: 13px; height: 13px;margin-right:5px;float:left;'></div><span>Ice House</span></div>"
+"<div><div style='border: 1px solid black; background-color: white; opacity: 1; border-radius: 50%; width: 13px; height: 13px;float:left; margin-top:-16px; margin-left: 20px'><span style='font-size: 12px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspIce&nbspHouse</span></div>"
 :
 IH,
-"<div class='event-type'><div style='border: 1px solid black; background-color: blue; opacity: 0.8; border-radius: 50%; width: 13px; height: 13px;margin-right:5px;float:left;'></div><span>FastTrac</span></div>"
+"<div><div style='border: 1px solid black; background-color: blue; opacity: 1; border-radius: 50%; width: 13px; height: 13px;float:left; margin-top:-16px; margin-left: 20px'><span style='font-size: 12px;'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbspFastTrac</span></div>"
 :
 FT,
-"<div class='event-type'><div style='border: 1px solid black; background-color: orange; opacity: 0.8; border-radius: 50%; width: 13px; height: 13px;margin-right:5px;float:left;'></div><span>1 Million Cups</span></div>"
+"<div><div style='border: 1px solid black; background-color: orange; opacity: 1; border-radius: 50%; width: 13px; height: 13px;float:left; margin-top:-16px; margin-left: 20px'></div><span style='font-size: 12px;float:left;margin-top:-16px;margin-left:20px'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp1&nbspMillion&nbspCups</span></div>"
 :
 MC}
 
