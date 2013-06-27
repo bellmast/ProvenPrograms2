42295b5301f06098fe82adbb407e6f21df9a128e
bellmast <jbm444@gmail.com>
2013-06-27 13:22:57 -0500

    Create istart.js

---
+++ istart.js
@@ -0,0 +1,1319 @@
+var iStart = {
+type: "FeatureCollection",
+features: [
+{
+type: "Feature",
+properties: {
+City: "Little Rock",
+State: "AR",
+Number: "3",
+unitId: "Little Rock",
+lat: "34.7464809",
+lon: "-92.2895948"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.2895948,
+34.7464809
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Phoenix",
+State: "AZ",
+Number: "8",
+unitId: "Phoenix",
+lat: "33.4483771",
+lon: "-112.0740373"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-112.0740373,
+33.4483771
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Davis",
+State: "CA",
+Number: "2",
+unitId: "Davis",
+lat: "38.5449065",
+lon: "-121.7405167"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-121.7405167,
+38.5449065
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Irvine",
+State: "CA",
+Number: "1",
+unitId: "Irvine",
+lat: "33.6839473",
+lon: "-117.7946942"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-117.7946942,
+33.6839473
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "LA",
+State: "CA",
+Number: "1",
+unitId: "LA",
+lat: "34.0522342",
+lon: "-118.2436849"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.2436849,
+34.0522342
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Oakland",
+State: "CA",
+Number: "2",
+unitId: "Oakland",
+lat: "37.8043637",
+lon: "-122.2711137"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.2711137,
+37.8043637
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Pasadena",
+State: "CA",
+Number: "2",
+unitId: "Pasadena",
+lat: "34.1477849",
+lon: "-118.1445155"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.1445155,
+34.1477849
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Rohnert Park",
+State: "CA",
+Number: "1",
+unitId: "Rohnert Park",
+lat: "38.3396367",
+lon: "-122.7010984"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.7010984,
+38.3396367
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "San Luis Obispo",
+State: "CA",
+Number: "1",
+unitId: "San Luis Obispo",
+lat: "35.2827524",
+lon: "-120.6596156"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-120.6596156,
+35.2827524
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Santa Barbara",
+State: "CA",
+Number: "2",
+unitId: "Santa Barbara",
+lat: "34.4208305",
+lon: "-119.6981901"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.6981901,
+34.4208305
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Santa Clara",
+State: "CA",
+Number: "4",
+unitId: "Santa Clara",
+lat: "37.3541079",
+lon: "-121.9552356"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-121.9552356,
+37.3541079
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "New Haven",
+State: "CT",
+Number: "1",
+unitId: "New Haven",
+lat: "41.3081527",
+lon: "-72.9281577"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-72.9281577,
+41.3081527
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Washington",
+State: "DC",
+Number: "10",
+unitId: "Washington",
+lat: "38.8951118",
+lon: "-77.0363658"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.0363658,
+38.8951118
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Boca Raton",
+State: "FL",
+Number: "3",
+unitId: "Boca Raton",
+lat: "26.3586885",
+lon: "-80.0830984"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.0830984,
+26.3586885
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Fort Pierce",
+State: "FL",
+Number: "1",
+unitId: "Fort Pierce",
+lat: "27.4467056",
+lon: "-80.3256056"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.3256056,
+27.4467056
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Miami",
+State: "FL",
+Number: "1",
+unitId: "Miami",
+lat: "25.7889689",
+lon: "-80.2264393"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.2264393,
+25.7889689
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Tallahassee",
+State: "FL",
+Number: "2",
+unitId: "Tallahassee",
+lat: "30.4382559",
+lon: "-84.2807329"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.2807329,
+30.4382559
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Chicago",
+State: "IL",
+Number: "6",
+unitId: "Chicago",
+lat: "41.8781136",
+lon: "-87.6297982"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.6297982,
+41.8781136
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Deerfield",
+State: "IL",
+Number: "2",
+unitId: "Deerfield",
+lat: "42.1711365",
+lon: "-87.8445119"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.8445119,
+42.1711365
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Normal",
+State: "IL",
+Number: "1",
+unitId: "Normal",
+lat: "40.5142026",
+lon: "-88.9906312"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.9906312,
+40.5142026
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Covington",
+State: "KY",
+Number: "1",
+unitId: "Covington",
+lat: "39.0836712",
+lon: "-84.5085536"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5085536,
+39.0836712
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Louisville",
+State: "KY",
+Number: "1",
+unitId: "Louisville",
+lat: "38.2526647",
+lon: "-85.7584557"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.7584557,
+38.2526647
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Baton Rouge",
+State: "LA",
+Number: "2",
+unitId: "Baton Rouge",
+lat: "30.4582829",
+lon: "-91.1403196"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-91.1403196,
+30.4582829
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "New Orleans",
+State: "LA",
+Number: "5",
+unitId: "New Orleans",
+lat: "29.9510658",
+lon: "-90.0715323"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.0715323,
+29.9510658
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Boston",
+State: "MA",
+Number: "24",
+unitId: "Boston",
+lat: "42.3584308",
+lon: "-71.0597732"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.0597732,
+42.3584308
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Cambridge",
+State: "MA",
+Number: "1",
+unitId: "Cambridge",
+lat: "42.3736158",
+lon: "-71.1097335"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.1097335,
+42.3736158
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Williamstown",
+State: "MA",
+Number: "1",
+unitId: "Williamstown",
+lat: "42.7120196",
+lon: "-73.2036898"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.2036898,
+42.7120196
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Baltimore",
+State: "MD",
+Number: "1",
+unitId: "Baltimore",
+lat: "39.2903848",
+lon: "-76.6121893"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.6121893,
+39.2903848
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "College Park",
+State: "MD",
+Number: "6",
+unitId: "College Park",
+lat: "38.9896967",
+lon: "-76.93776"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.93776,
+38.9896967
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Portland",
+State: "ME",
+Number: "2",
+unitId: "Portland",
+lat: "43.661471",
+lon: "-70.2553259"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-70.2553259,
+43.661471
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Grand Rapids",
+State: "MI",
+Number: "1",
+unitId: "Grand Rapids",
+lat: "42.9633599",
+lon: "-85.6680863"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.6680863,
+42.9633599
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Kansas City",
+State: "MO",
+Number: "13",
+unitId: "Kansas City",
+lat: "39.0997265",
+lon: "-94.5785667"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Lee's Summit",
+State: "MO",
+Number: "1",
+unitId: "Lee's Summit",
+lat: "38.916343",
+lon: "-94.383516"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.383516,
+38.916343
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Marshall",
+State: "MO",
+Number: "2",
+unitId: "Marshall",
+lat: "39.1230777",
+lon: "-93.1968704"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.1968704,
+39.1230777
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "St. Louis",
+State: "MO",
+Number: "1",
+unitId: "St. Louis",
+lat: "38.6270025",
+lon: "-90.1994042"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.1994042,
+38.6270025
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Missoula",
+State: "MT",
+Number: "1",
+unitId: "Missoula",
+lat: "46.8605189",
+lon: "-114.019501"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-114.019501,
+46.8605189
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Chapel Hill",
+State: "NC",
+Number: "2",
+unitId: "Chapel Hill",
+lat: "35.9131996",
+lon: "-79.0558445"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.0558445,
+35.9131996
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Charlotte",
+State: "NC",
+Number: "1",
+unitId: "Charlotte",
+lat: "35.2270869",
+lon: "-80.8431267"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.8431267,
+35.2270869
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Durham",
+State: "NC",
+Number: "3",
+unitId: "Durham",
+lat: "35.9940329",
+lon: "-78.898619"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.898619,
+35.9940329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Greensboro",
+State: "NC",
+Number: "3",
+unitId: "Greensboro",
+lat: "36.0726354",
+lon: "-79.7919754"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.7919754,
+36.0726354
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Raleigh",
+State: "NC",
+Number: "1",
+unitId: "Raleigh",
+lat: "35.772096",
+lon: "-78.6386145"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.6386145,
+35.772096
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Collingswood",
+State: "NJ",
+Number: "1",
+unitId: "Collingswood",
+lat: "39.9181686",
+lon: "-75.071284"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.071284,
+39.9181686
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Jersey City",
+State: "NJ",
+Number: "1",
+unitId: "Jersey City",
+lat: "40.7281575",
+lon: "-74.0776417"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.0776417,
+40.7281575
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Mullica Hill",
+State: "NJ",
+Number: "7",
+unitId: "Mullica Hill",
+lat: "39.7392804",
+lon: "-75.2240718"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.2240718,
+39.7392804
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "South Orange",
+State: "NJ",
+Number: "1",
+unitId: "South Orange",
+lat: "40.7489277",
+lon: "-74.2609903"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.2609903,
+40.7489277
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Incline Village",
+State: "NV",
+Number: "1",
+unitId: "Incline Village",
+lat: "39.26721",
+lon: "-119.963988"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.963988,
+39.26721
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Reno",
+State: "NV",
+Number: "1",
+unitId: "Reno",
+lat: "39.5296329",
+lon: "-119.8138027"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.8138027,
+39.5296329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "New York",
+State: "NY",
+Number: "3",
+unitId: "New York",
+lat: "40.7143528",
+lon: "-74.0059731"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.0059731,
+40.7143528
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Canton",
+State: "OH",
+Number: "1",
+unitId: "Canton",
+lat: "40.7989473",
+lon: "-81.378447"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.378447,
+40.7989473
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Cincinnati",
+State: "OH",
+Number: "1",
+unitId: "Cincinnati",
+lat: "39.1031182",
+lon: "-84.5120196"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5120196,
+39.1031182
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Columbus",
+State: "OH",
+Number: "1",
+unitId: "Columbus",
+lat: "39.9611755",
+lon: "-82.9987942"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.9987942,
+39.9611755
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Streetsboro",
+State: "OH",
+Number: "2",
+unitId: "Streetsboro",
+lat: "41.2392227",
+lon: "-81.3459405"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.3459405,
+41.2392227
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Norman",
+State: "OK",
+Number: "1",
+unitId: "Norman",
+lat: "35.2225668",
+lon: "-97.4394777"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.4394777,
+35.2225668
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Portland",
+State: "OR",
+Number: "3",
+unitId: "Portland",
+lat: "45.5234515",
+lon: "-122.6762071"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.6762071,
+45.5234515
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Bethlehem",
+State: "PA",
+Number: "2",
+unitId: "Bethlehem",
+lat: "40.6259316",
+lon: "-75.3704579"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.3704579,
+40.6259316
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Pittsburg",
+State: "PA",
+Number: "1",
+unitId: "Pittsburg",
+lat: "40.4406248",
+lon: "-79.9958864"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.9958864,
+40.4406248
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Reading",
+State: "PA",
+Number: "1",
+unitId: "Reading",
+lat: "40.3356483",
+lon: "-75.9268747"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.9268747,
+40.3356483
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Shippensburg",
+State: "PA",
+Number: "4",
+unitId: "Shippensburg",
+lat: "40.0506453",
+lon: "-77.5202647"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.5202647,
+40.0506453
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "West Chester",
+State: "PA",
+Number: "2",
+unitId: "West Chester",
+lat: "39.9606643",
+lon: "-75.6054882"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.6054882,
+39.9606643
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Columbia",
+State: "SC",
+Number: "2",
+unitId: "Columbia",
+lat: "34.0007104",
+lon: "-81.0348144"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.0348144,
+34.0007104
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Arlington",
+State: "TX",
+Number: "1",
+unitId: "Arlington",
+lat: "32.735687",
+lon: "-97.1080656"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.1080656,
+32.735687
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Burkburnett",
+State: "TX",
+Number: "1",
+unitId: "Burkburnett",
+lat: "34.0978711",
+lon: "-98.5706134"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-98.5706134,
+34.0978711
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Houston",
+State: "TX",
+Number: "6",
+unitId: "Houston",
+lat: "29.7601927",
+lon: "-95.3693896"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.3693896,
+29.7601927
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Waco",
+State: "TX",
+Number: "2",
+unitId: "Waco",
+lat: "31.549333",
+lon: "-97.1466695"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.1466695,
+31.549333
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Alexandria",
+State: "VA",
+Number: "1",
+unitId: "Alexandria",
+lat: "38.8048355",
+lon: "-77.0469214"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.0469214,
+38.8048355
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Blacksburg",
+State: "VA",
+Number: "1",
+unitId: "Blacksburg",
+lat: "37.2295733",
+lon: "-80.4139393"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.4139393,
+37.2295733
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Charlottesville",
+State: "VA",
+Number: "1",
+unitId: "Charlottesville",
+lat: "38.0293059",
+lon: "-78.4766781"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.4766781,
+38.0293059
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Seattle",
+State: "WA",
+Number: "2",
+unitId: "Seattle",
+lat: "47.6062095",
+lon: "-122.3320708"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.3320708,
+47.6062095
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Spokane",
+State: "WA",
+Number: "1",
+unitId: "Spokane",
+lat: "47.6587802",
+lon: "-117.4260466"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-117.4260466,
+47.6587802
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Maidson",
+State: "WI",
+Number: "1",
+unitId: "Maidson",
+lat: "43.0730517",
+lon: "-89.4012302"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.4012302,
+43.0730517
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Milwaukee",
+State: "WI",
+Number: "1",
+unitId: "Milwaukee",
+lat: "43.0389025",
+lon: "-87.9064736"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.9064736,
+43.0389025
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Neenah",
+State: "WI",
+Number: "1",
+unitId: "Neenah",
+lat: "44.1858193",
+lon: "-88.462609"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.462609,
+44.1858193
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Oshkosh",
+State: "WI",
+Number: "2",
+unitId: "Oshkosh",
+lat: "44.0247062",
+lon: "-88.5426136"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.5426136,
+44.0247062
+]
+}
+}
+]
+}
