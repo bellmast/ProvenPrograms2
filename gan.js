4ad7a84ceb35d49547461b336b1fb4994b002bb1
bellmast <jbm444@gmail.com>
2013-06-27 13:22:37 -0500

    Create gan.js

---
+++ gan.js
@@ -0,0 +1,481 @@
+var globalAN = {
+type: "FeatureCollection",
+features: [
+{
+type: "Feature",
+properties: {
+City: "Santa Monica",
+lat: "34.0194543",
+lon: "-118.4911912",
+State: "CA",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.4911912,
+34.0194543
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Chicago",
+lat: "41.8781136",
+lon: "-87.6297982",
+State: "IL",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.6297982,
+41.8781136
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Greenville",
+lat: "34.8526176",
+lon: "-82.3940104",
+State: "SC",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.3940104,
+34.8526176
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Dallas",
+lat: "32.802955",
+lon: "-96.769923",
+State: "TX",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.769923,
+32.802955
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Providence",
+lat: "41.8239891",
+lon: "-71.4128343",
+State: "RI",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.4128343,
+41.8239891
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "San Francisco",
+lat: "37.7749295",
+lon: "-122.4194155",
+State: "CA",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.4194155,
+37.7749295
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Boulder",
+lat: "40.0149856",
+lon: "-105.2705456",
+State: "CO",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.2705456,
+40.0149856
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Columbus",
+lat: "39.9611755",
+lon: "-82.9987942",
+State: "OH",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.9987942,
+39.9611755
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Pittsburgh",
+lat: "40.4406248",
+lon: "-79.9958864",
+State: "PA",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.9958864,
+40.4406248
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Nashville",
+lat: "36.1666667",
+lon: "-86.7833333",
+State: "TN",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.7833333,
+36.1666667
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Scottsdale",
+lat: "33.4941704",
+lon: "-111.9260519",
+State: "AZ",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.9260519,
+33.4941704
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "New York",
+lat: "40.7143528",
+lon: "-74.0059731",
+State: "NY",
+Numberz: "2"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.0059731,
+40.7143528
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Los Angeles",
+lat: "34.0522342",
+lon: "-118.2436849",
+State: "CA",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.2436849,
+34.0522342
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "San Antonio",
+lat: "29.4241219",
+lon: "-98.4936282",
+State: "TX",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-98.4936282,
+29.4241219
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Tampa",
+lat: "27.950575",
+lon: "-82.4571776",
+State: "FL",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.4571776,
+27.950575
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Syracuse",
+lat: "43.0481221",
+lon: "-76.1474244",
+State: "NY",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.1474244,
+43.0481221
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Miami",
+lat: "25.7889689",
+lon: "-80.2264393",
+State: "FL",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.2264393,
+25.7889689
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Orlando",
+lat: "28.5383355",
+lon: "-81.3792365",
+State: "FL",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.3792365,
+28.5383355
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Saint Louis",
+lat: "38.6270025",
+lon: "-90.1994042",
+State: "MO",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.1994042,
+38.6270025
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Fayetteville",
+lat: "36.0625795",
+lon: "-94.1574263",
+State: "AR",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.1574263,
+36.0625795
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Houston",
+lat: "29.7601927",
+lon: "-95.3693896",
+State: "TX",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.3693896,
+29.7601927
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Madison",
+lat: "43.0730517",
+lon: "-89.4012302",
+State: "WI",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.4012302,
+43.0730517
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Cincinnati",
+lat: "39.1031182",
+lon: "-84.5120196",
+State: "OH",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5120196,
+39.1031182
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Boston",
+lat: "42.3584308",
+lon: "-71.0597732",
+State: "MA",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.0597732,
+42.3584308
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "New Orleans",
+lat: "29.9510658",
+lon: "-90.0715323",
+State: "LA",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.0715323,
+29.9510658
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Salt Lake City",
+lat: "40.7607793",
+lon: "-111.8910474",
+State: "UT",
+Numberz: "1"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.8910474,
+40.7607793
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Milwaukee",
+lat: "43.0389025",
+lon: "-87.9064736",
+State: "WI",
+Numberz: "2"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.9064736,
+43.0389025
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Seattle",
+lat: "47.6062095",
+lon: "-122.3320708",
+State: "WA",
+Numberz: "2"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.3320708,
+47.6062095
+]
+}
+}
+]
+}
