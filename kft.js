de08c1ee822445510fd82aec244b54c382f1e6fd
bellmast <jbm444@gmail.com>
2013-06-27 11:36:27 -0500

    Create kft.js

---
+++ kft.js
@@ -0,0 +1,1285 @@
+var KFT = {
+type: "FeatureCollection",
+features: [
+{
+type: "Feature",
+properties: {
+Org: "American Business Women's Association",
+lat: "38.9221969",
+lon: "-94.6295394",
+Blended: "80"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.6295394,
+38.9221969
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "American Chemical Society",
+lat: "38.9080786",
+lon: "-77.0406147",
+Blended: "100"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.0406147,
+38.9080786
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "American Public University",
+lat: "38.806939",
+lon: "-77.51169",
+Blended: "23"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.51169,
+38.806939
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Austen BioInnovation Institution in Akron",
+lat: "41.0802154",
+lon: "-81.5198692",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.5198692,
+41.0802154
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Barcel USA (GrupoBimbo Bakeries)",
+lat: "48.88235",
+lon: "2.3818222",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+2.3818222,
+48.88235
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Bethany College",
+lat: "38.58006",
+lon: "-97.672985",
+Blended: "10"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.672985,
+38.58006
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Blue Valley CAPS",
+lat: "-34.02115",
+lon: "18.381092",
+Blended: "100"
+},
+geometry: {
+type: "Point",
+coordinates: [
+18.381092,
+-34.02115
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Broward County Office of Economic and Small Business Development",
+lat: "26.1242719",
+lon: "-80.128901",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.128901,
+26.1242719
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Business Ownership Initiative of Indiana",
+lat: "39.7713425",
+lon: "-86.1573709",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.1573709,
+39.7713425
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "California State University Chico PEV",
+lat: "39.735769",
+lon: "-121.8504463",
+Blended: "5"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-121.8504463,
+39.735769
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Center for Entrepreneurship",
+lat: "29.7203259",
+lon: "-95.3435062",
+Blended: "100"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.3435062,
+29.7203259
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Central TX College Bookstore",
+lat: "29.4140719",
+lon: "-98.455047",
+Blended: "7"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-98.455047,
+29.4140719
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "City Startup Labs, Inc.",
+lat: "35.2326781",
+lon: "-80.8460822",
+Blended: "25"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.8460822,
+35.2326781
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Delaware Economic Development Office",
+lat: "39.160539",
+lon: "-75.523206",
+Blended: "100"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.523206,
+39.160539
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Delaware Technical Community College",
+lat: "38.6968566",
+lon: "-75.4070945",
+Blended: "45"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.4070945,
+38.6968566
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "District of Columbia",
+lat: "38.912068",
+lon: "-77.0190228",
+Blended: "130"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.0190228,
+38.912068
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "East Central University PEV",
+lat: "34.8101393",
+lon: "-96.7065914",
+Blended: "25"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.7065914,
+34.8101393
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Eastern Shore Entrepreneurship Center",
+lat: "38.7742099",
+lon: "-76.0764036",
+Blended: "25"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.0764036,
+38.7742099
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Ernest and Hardy LLC",
+lat: "33.9346756",
+lon: "-84.5955732",
+Blended: "200"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5955732,
+33.9346756
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Fox Valley Technical College PEV",
+lat: "44.0291228",
+lon: "-88.4984991",
+Blended: "14"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.4984991,
+44.0291228
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Garrett College",
+lat: "39.5618972",
+lon: "-79.3402189",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.3402189,
+39.5618972
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Go Topeka Economic Partnership",
+lat: "39.0555831",
+lon: "-95.6747209",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.6747209,
+39.0555831
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Grand Rapids CC Bookstore FL 505 PEV",
+lat: "42.9616689",
+lon: "-85.6588999",
+Blended: "16"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.6588999,
+42.9616689
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Hebrew Free Loan Society",
+lat: "40.7519846",
+lon: "-73.9697795",
+Blended: "23"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.9697795,
+40.7519846
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "High Growth Partners S.G.E.C.R.S.A.",
+lat: "0",
+lon: "0",
+Blended: "65"
+},
+geometry: {
+type: "Point",
+coordinates: [
+0,
+0
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Innova Partners LLC",
+lat: "37.5410261",
+lon: "-77.4386429",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.4386429,
+37.5410261
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Innovation Accelerator",
+lat: "41.2354329",
+lon: "-95.9938339",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.9938339,
+41.2354329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Innovation Norway",
+lat: "51.5084199",
+lon: "-0.1338223",
+Blended: "25"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-0.1338223,
+51.5084199
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Jamestown Community College",
+lat: "42.114891",
+lon: "-79.219197",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.219197,
+42.114891
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Kansas Women's Business Center",
+lat: "38.9524519",
+lon: "-94.718659",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.718659,
+38.9524519
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Kauffman FastTrac, Inc.",
+lat: "39.0362552",
+lon: "-94.5753394",
+Blended: "118"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5753394,
+39.0362552
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Korea Entrepreneurship Foundation",
+lat: "0",
+lon: "0",
+Blended: "300"
+},
+geometry: {
+type: "Point",
+coordinates: [
+0,
+0
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Ladlas Prince",
+lat: "42.642381",
+lon: "-83.277359",
+Blended: "30"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-83.277359,
+42.642381
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Lansing Community College",
+lat: "42.7374222",
+lon: "-84.5545403",
+Blended: "3"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5545403,
+42.7374222
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Lewis & Clark Research Institute",
+lat: "39.2758776",
+lon: "-94.9974461",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.9974461,
+39.2758776
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "LifeScience Alley",
+lat: "49.5199316",
+lon: "-96.6624321",
+Blended: "200"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.6624321,
+49.5199316
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Medaille College PEV",
+lat: "42.9734553",
+lon: "-78.713608",
+Blended: "34"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.713608,
+42.9734553
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Meta Group",
+lat: "0",
+lon: "0",
+Blended: "267"
+},
+geometry: {
+type: "Point",
+coordinates: [
+0,
+0
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Mi Casa Resource Center",
+lat: "39.7007844",
+lon: "-105.0031457",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.0031457,
+39.7007844
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Miami Innovation Center for Entrepreneurship Inc.",
+lat: "25.76528",
+lon: "-80.1847167",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.1847167,
+25.76528
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Mid America Nazarene University",
+lat: "38.8760708",
+lon: "-94.7823593",
+Blended: "250"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.7823593,
+38.8760708
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Midlands Technical College",
+lat: "33.9880108",
+lon: "-80.9823225",
+Blended: "75"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.9823225,
+33.9880108
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Mississippi State University",
+lat: "33.45622",
+lon: "-88.795708",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.795708,
+33.45622
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Missouri SBTDC",
+lat: "38.9331391",
+lon: "-92.3738037",
+Blended: "100"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.3738037,
+38.9331391
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Montgomery County College",
+lat: "39.1033053",
+lon: "-77.1981184",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.1981184,
+39.1033053
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Nanyang Technological University",
+lat: "1.3428703",
+lon: "103.6813512",
+Blended: "500"
+},
+geometry: {
+type: "Point",
+coordinates: [
+103.6813512,
+1.3428703
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "National Marketing and Learning",
+lat: "38.9791799",
+lon: "-94.3667907",
+Blended: "100"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.3667907,
+38.9791799
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Northeast Wisconsin Technical College",
+lat: "44.524927",
+lon: "-88.106939",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.106939,
+44.524927
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Northern Virginia Technology Council (NVTC)",
+lat: "37.2232316",
+lon: "-81.4498717",
+Blended: "13"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.4498717,
+37.2232316
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Oklahoma Baptist University PEV",
+lat: "35.38837",
+lon: "-96.9423844",
+Blended: "8"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.9423844,
+35.38837
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "P/Strada LLC",
+lat: "39.0587452",
+lon: "-94.5985613",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5985613,
+39.0587452
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Parkland College",
+lat: "40.1339563",
+lon: "-88.2886893",
+Blended: "15"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.2886893,
+40.1339563
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Propero HCA, LLC",
+lat: "40.7686973",
+lon: "-73.9918181",
+Blended: "180"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.9918181,
+40.7686973
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Puerto Rico SBTDC",
+lat: "0",
+lon: "0",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+0,
+0
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Right Management - Overland Park, KS",
+lat: "38.9193108",
+lon: "-94.70189",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.70189,
+38.9193108
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Rocky Mountain Innosphere",
+lat: "40.6358803",
+lon: "-105.0089888",
+Blended: "25"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.0089888,
+40.6358803
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "S.W. Louisiana Economic Development Alliance",
+lat: "30.226137",
+lon: "-93.219142",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.219142,
+30.226137
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Seedco",
+lat: "35.1543632",
+lon: "-90.0606637",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.0606637,
+35.1543632
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Snow College SBDC",
+lat: "31.9987234",
+lon: "-110.2859656",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-110.2859656,
+31.9987234
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Southeastern Louisiana Economic Development Alliance",
+lat: "30.2293526",
+lon: "-93.2131232",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.2131232,
+30.2293526
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Tampa Bay Innovation Center",
+lat: "27.8724191",
+lon: "-82.7469944",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.7469944,
+27.8724191
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Tech Coast Venture Network",
+lat: "33.6588951",
+lon: "-117.8282121",
+Blended: "21"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-117.8282121,
+33.6588951
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "The 10X Group",
+lat: "38.4692521",
+lon: "-122.7199012",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.7199012,
+38.4692521
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "The Levin Institute",
+lat: "40.7593941",
+lon: "-73.9697795",
+Blended: "750"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.9697795,
+40.7593941
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Towson University Small Business Development Center",
+lat: "39.3929109",
+lon: "-76.6080554",
+Blended: "30"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.6080554,
+39.3929109
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of California, Merced SBDC Regional Network",
+lat: "36.8179331",
+lon: "-119.7599761",
+Blended: "75"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.7599761,
+36.8179331
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Central Florida",
+lat: "28.6024276",
+lon: "-81.2000599",
+Blended: "5"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.2000599,
+28.6024276
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Central Missouri",
+lat: "38.929098",
+lon: "-94.398313",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.398313,
+38.929098
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Georgia Small Business Development Center",
+lat: "31.578917",
+lon: "-84.150403",
+Blended: "38"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.150403,
+31.578917
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Illinois at Urbana-Champaign",
+lat: "40.1027158",
+lon: "-88.231957",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.231957,
+40.1027158
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Missouri at Kansas City Small Business and Technology Center",
+lat: "39.0997265",
+lon: "-94.5785667",
+Blended: "137"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Montana - MonTEC",
+lat: "46.8608037",
+lon: "-113.9820267",
+Blended: "10"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-113.9820267,
+46.8608037
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Southern Indiana",
+lat: "37.9582278",
+lon: "-87.6753726",
+Blended: "28"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.6753726,
+37.9582278
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "University of Texas, Brownsville",
+lat: "25.8997711",
+lon: "-97.4739755",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.4739755,
+25.8997711
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Urban League of Greater Cincinnati",
+lat: "39.1540971",
+lon: "-84.485507",
+Blended: "60"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.485507,
+39.1540971
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Vail Leadership Institute",
+lat: "39.595736",
+lon: "-106.5230542",
+Blended: "40"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-106.5230542,
+39.595736
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "West Texas Innovation Network",
+lat: "32.3249863",
+lon: "-99.6564092",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-99.6564092,
+32.3249863
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "William Jewell College",
+lat: "39.2489693",
+lon: "-94.4117789",
+Blended: "10"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.4117789,
+39.2489693
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Women's Business Enterprise Alliance",
+lat: "29.8265052",
+lon: "-95.4672962",
+Blended: "25"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.4672962,
+29.8265052
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Org: "Wyoming County Business Center",
+lat: "42.7555047",
+lon: "-77.9967043",
+Blended: "20"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.9967043,
+42.7555047
+]
+}
+}
+]
+}
