d32ddbc6f0e78a8ea58f7f95bf1ca7030fd23af7
bellmast <jbm444@gmail.com>
2013-08-28 16:12:58 -0500

    Update 1MC.js

--- 1MC.js
+++ 1MC.js
@@ -1,23 +1,229 @@
 var oMC = {
-"type": "FeatureCollection",
-"features": [
-{ "type": "Feature", "properties": { "City": "Denver ", "State": "CO", "lat": "39.737567", "lon": "-104.9847179" }, "geometry": { "type": "Point", "coordinates": [ -104.9847179, 39.737567 ] } }
-,
-{ "type": "Feature", "properties": { "City": "Cedar Rapids", "State": "IA", "lat": "41.9778795", "lon": "-91.6656232" }, "geometry": { "type": "Point", "coordinates": [ -91.6656232, 41.9778795 ] } }
-,
-{ "type": "Feature", "properties": { "City": "West Des Moines", "State": "IA", "lat": "41.5772115", "lon": "-93.711332" }, "geometry": { "type": "Point", "coordinates": [ -93.711332, 41.5772115 ] } }
-,
-{ "type": "Feature", "properties": { "City": "Kansas City", "State": "MO", "lat": "39.0997265", "lon": "-94.5785667" }, "geometry": { "type": "Point", "coordinates": [ -94.5785667, 39.0997265 ] } }
-,
-{ "type": "Feature", "properties": { "City": "St. Louis", "State": "MO", "lat": "38.6270025", "lon": "-90.1994042" }, "geometry": { "type": "Point", "coordinates": [ -90.1994042, 38.6270025 ] } }
-,
-{ "type": "Feature", "properties": { "City": "Chapel Hill", "State": "NC", "lat": "35.9131996", "lon": "-79.0558445" }, "geometry": { "type": "Point", "coordinates": [ -79.0558445, 35.9131996 ] } }
-,
-{ "type": "Feature", "properties": { "City": "Reno", "State": "NV", "lat": "39.5296329", "lon": "-119.8138027" }, "geometry": { "type": "Point", "coordinates": [ -119.8138027, 39.5296329 ] } }
-,
-{ "type": "Feature", "properties": { "City": "Chattanooga", "State": "TN", "lat": "35.0456297", "lon": "-85.3096801" }, "geometry": { "type": "Point", "coordinates": [ -85.3096801, 35.0456297 ] } }
-,
-{ "type": "Feature", "properties": { "City": "Houston", "State": "TX", "lat": "29.7601927", "lon": "-95.3693896" }, "geometry": { "type": "Point", "coordinates": [ -95.3693896, 29.7601927 ] } }
-
+type: "FeatureCollection",
+features: [
+{
+type: "Feature",
+properties: {
+City: "Kansas City",
+State: "MO",
+lat: "39.0997265",
+lon: "-94.5785667"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Cedar Rapids",
+State: "IA",
+lat: "41.9778795",
+lon: "-91.6656232"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-91.6656232,
+41.9778795
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Chapel Hill",
+State: "NC",
+lat: "35.9131996",
+lon: "-79.0558445"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.0558445,
+35.9131996
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Chattanooga",
+State: "TN",
+lat: "35.0456297",
+lon: "-85.3096801"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.3096801,
+35.0456297
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Columbia",
+State: "MO",
+lat: "38.9517053",
+lon: "-92.3340724"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.3340724,
+38.9517053
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Georgetown",
+State: "DE",
+lat: "38.6901134",
+lon: "-75.3854728"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.3854728,
+38.6901134
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Denver",
+State: "CO",
+lat: "39.737567",
+lon: "-104.9847179"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-104.9847179,
+39.737567
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Des Moines",
+State: "IA",
+lat: "41.6005448",
+lon: "-93.6091064"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.6091064,
+41.6005448
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Houston",
+State: "TX",
+lat: "29.7601927",
+lon: "-95.3693896"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.3693896,
+29.7601927
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Iowa City",
+State: "IA",
+lat: "41.6611277",
+lon: "-91.5301683"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-91.5301683,
+41.6611277
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Lawrence",
+State: "KS",
+lat: "38.9716689",
+lon: "-95.2352501"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.2352501,
+38.9716689
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Orlando",
+State: "FL",
+lat: "28.5383355",
+lon: "-81.3792365"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.3792365,
+28.5383355
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Reno",
+State: "NV",
+lat: "39.5296329",
+lon: "-119.8138027"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.8138027,
+39.5296329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "St. Louis",
+State: "MO",
+lat: "38.6270025",
+lon: "-90.1994042"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.1994042,
+38.6270025
+]
+}
+}
 ]
 }
