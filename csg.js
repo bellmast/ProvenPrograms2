9536a163ff1c22ee05469272ac5ba89a9312bb48
bellmast <jbm444@gmail.com>
2013-09-16 12:04:31 -0500

    Create csg.js

---
+++ csg.js
@@ -0,0 +1,1125 @@
+{
+type: "FeatureCollection",
+features: [
+{
+type: "Feature",
+properties: {
+Place: "Central Wyoming",
+State: "WY",
+lat: "41.1459767",
+lon: "-104.8225607"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-104.8225607,
+41.1459767
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Montgomery",
+State: " AL",
+lat: "32.3668052",
+lon: "-86.2999689"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.2999689,
+32.3668052
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Mobile",
+State: " AL",
+lat: "30.6943566",
+lon: "-88.0430541"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-88.0430541,
+30.6943566
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Anchorage",
+State: " AK",
+lat: "61.2180556",
+lon: "-149.9002778"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-149.9002778,
+61.2180556
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban Phoenix",
+State: "AZ",
+lat: "18.9576061",
+lon: "72.8189135"
+},
+geometry: {
+type: "Point",
+coordinates: [
+72.8189135,
+18.9576061
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Tucson",
+State: " AZ",
+lat: "32.2217429",
+lon: "-110.926479"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-110.926479,
+32.2217429
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Central",
+State: " AR",
+lat: "-25.5628115",
+lon: "-57.5079912"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-57.5079912,
+-25.5628115
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Rogers",
+State: " AR",
+lat: "36.3320196",
+lon: "-94.1185366"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.1185366,
+36.3320196
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "South Bay Los Angeles",
+State: "CA",
+lat: "33.8798386",
+lon: "-118.3812562"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.3812562,
+33.8798386
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Oxnard-Thousand Oaks",
+State: " CA",
+lat: "34.2078567",
+lon: "-119.1390642"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.1390642,
+34.2078567
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Fort Collins and Greeley",
+State: " CO",
+lat: "40.5852602",
+lon: "-105.084423"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.084423,
+40.5852602
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Boulder",
+State: " CO",
+lat: "40.0149856",
+lon: "-105.2705456"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.2705456,
+40.0149856
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Central/south CT",
+State: "CT",
+lat: "41.9836332",
+lon: "-72.4475758"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-72.4475758,
+41.9836332
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Waterford",
+State: " CT",
+lat: "41.3542395",
+lon: "-72.164816"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-72.164816,
+41.3542395
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Northwestern DE",
+State: "DE",
+lat: "58.3659277",
+lon: "39.5271758"
+},
+geometry: {
+type: "Point",
+coordinates: [
+39.5271758,
+58.3659277
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Tampa",
+State: " FL",
+lat: "27.950575",
+lon: "-82.4571776"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.4571776,
+27.950575
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Broward County",
+State: " FL",
+lat: "26.190096",
+lon: "-80.365865"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.365865,
+26.190096
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Atlanta",
+State: "GA",
+lat: "33.7489954",
+lon: "-84.3879824"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.3879824,
+33.7489954
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Savannah",
+State: " GA",
+lat: "32.0835407",
+lon: "-81.0998342"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.0998342,
+32.0835407
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Maui",
+State: " HI",
+lat: "20.7983626",
+lon: "-156.3319253"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-156.3319253,
+20.7983626
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Oahu/Honolulu",
+State: "HI",
+lat: "21.4389123",
+lon: "-158.0000565"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-158.0000565,
+21.4389123
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Coeur d'Alene",
+State: " ID",
+lat: "47.6776832",
+lon: "-116.7804664"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-116.7804664,
+47.6776832
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban Chicago",
+State: "IL",
+lat: "41.989451",
+lon: "-87.748873"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.748873,
+41.989451
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Aberdeen",
+State: "SD",
+lat: "45.4646985",
+lon: "-98.4864829"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-98.4864829,
+45.4646985
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Central Wisconsin",
+State: "WI",
+lat: "44.7840503",
+lon: "-89.6724508"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.6724508,
+44.7840503
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban Milwaukee",
+State: "WI",
+lat: "43.1743953",
+lon: "-87.9090269"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.9090269,
+43.1743953
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Newark",
+State: " NJ",
+lat: "40.735657",
+lon: "-74.1723667"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.1723667,
+40.735657
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Western New Hampshire",
+State: "NH",
+lat: "43.1623502",
+lon: "-71.8485108"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.8485108,
+43.1623502
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Concord",
+State: " NH",
+lat: "43.2081366",
+lon: "-71.5375718"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.5375718,
+43.2081366
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Las Vegas",
+State: "",
+lat: "36.114646",
+lon: "-115.172816"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-115.172816,
+36.114646
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Lincoln",
+State: " NE",
+lat: "40.806862",
+lon: "-96.681679"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.681679,
+40.806862
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban St. Louis",
+State: "MO",
+lat: "38.6646479",
+lon: "-90.2967055"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.2967055,
+38.6646479
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "St. Charles County",
+State: " MO",
+lat: "38.7338885",
+lon: "-90.8294002"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.8294002,
+38.7338885
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Southern Minnesota",
+State: "MN",
+lat: "46.7621622",
+lon: "-92.0938012"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.0938012,
+46.7621622
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Western Michigan",
+State: "MI",
+lat: "42.2116543",
+lon: "-86.2762612"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.2762612,
+42.2116543
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Northeastern Massachusetts",
+State: "MA",
+lat: "42.3391987",
+lon: "-71.0908823"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.0908823,
+42.3391987
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Framingham",
+State: "MA",
+lat: "42.279286",
+lon: "-71.4161565"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.4161565,
+42.279286
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Northeastern Maine",
+State: "ME",
+lat: "43.7843147",
+lon: "-70.5710649"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-70.5710649,
+43.7843147
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban New Orleans",
+State: "LA",
+lat: "30.0204792",
+lon: "-89.962846"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.962846,
+30.0204792
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Baton Rouge",
+State: "LA",
+lat: "30.4582829",
+lon: "-91.1403196"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-91.1403196,
+30.4582829
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban Cincinnati",
+State: "KY",
+lat: "37.8393332",
+lon: "-84.2700179"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.2700179,
+37.8393332
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Salina",
+State: " KS",
+lat: "38.8402805",
+lon: "-97.6114237"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.6114237,
+38.8402805
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Johnson County",
+State: " KS",
+lat: "38.845412",
+lon: "-94.8520636"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.8520636,
+38.845412
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Ottumwa",
+State: " IA",
+lat: "41.0160293",
+lon: "-92.4083022"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.4083022,
+41.0160293
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Indianapolis and suburbs",
+State: "IN",
+lat: "34.0138274",
+lon: "-118.4420796"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.4420796,
+34.0138274
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Evansville",
+State: " IN",
+lat: "37.9715592",
+lon: "-87.5710898"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.5710898,
+37.9715592
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Knoxville and Maryville",
+State: " TN",
+lat: "35.8536149",
+lon: "-84.0167423"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.0167423,
+35.8536149
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Huntington",
+State: " WV",
+lat: "38.4192496",
+lon: "-82.445154"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.445154,
+38.4192496
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Greenville",
+State: " SC",
+lat: "34.8526176",
+lon: "-82.3940104"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.3940104,
+34.8526176
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Florence",
+State: " SC",
+lat: "34.1954331",
+lon: "-79.7625625"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.7625625,
+34.1954331
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Newport",
+State: " RI",
+lat: "41.4901024",
+lon: "-71.3128285"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-71.3128285,
+41.4901024
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "South PA",
+State: "PA",
+lat: "41.2033216",
+lon: "-77.1945247"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.1945247,
+41.2033216
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Scranton",
+State: " PA",
+lat: "41.408969",
+lon: "-75.6624122"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.6624122,
+41.408969
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Beaverton",
+State: " OR",
+lat: "45.487062",
+lon: "-122.8037102"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.8037102,
+45.487062
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Tulsa",
+State: " OK",
+lat: "36.1539816",
+lon: "-95.992775"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.992775,
+36.1539816
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Fargo",
+State: " ND",
+lat: "46.8771863",
+lon: "-96.7898034"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.7898034,
+46.8771863
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Asheville",
+State: " NC",
+lat: "35.5950581",
+lon: "-82.5514869"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.5514869,
+35.5950581
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Wake County",
+State: " NC",
+lat: "35.8031909",
+lon: "-78.5660852"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.5660852,
+35.8031909
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Nassau County",
+State: " NY",
+lat: "40.6546145",
+lon: "-73.5594128"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.5594128,
+40.6546145
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Las Cruces",
+State: " NM",
+lat: "32.3199396",
+lon: "-106.7636538"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-106.7636538,
+32.3199396
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Central NM",
+State: "",
+lat: "32.7764644",
+lon: "-108.1544888"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-108.1544888,
+32.7764644
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Union",
+State: " NJ",
+lat: "40.6975898",
+lon: "-74.2631635"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.2631635,
+40.6975898
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Camden",
+State: " NJ",
+lat: "39.9259463",
+lon: "-75.1196199"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.1196199,
+39.9259463
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Austin",
+State: "TX",
+lat: "30.267153",
+lon: "-97.7430608"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.7430608,
+30.267153
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Houston",
+State: "TX",
+lat: "29.7601927",
+lon: "-95.3693896"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.3693896,
+29.7601927
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Salt Lake City",
+State: "UT",
+lat: "40.7607793",
+lon: "-111.8910474"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.8910474,
+40.7607793
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Southwestern Vermont",
+State: "VT",
+lat: "42.874525",
+lon: "-73.2075833"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.2075833,
+42.874525
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Burlington",
+State: " VT",
+lat: "44.4758825",
+lon: "-73.212072"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.212072,
+44.4758825
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Fredericksburg",
+State: " VA",
+lat: "38.3031837",
+lon: "-77.4605399"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.4605399,
+38.3031837
+]
+}
+},
+{
+type: "Feature",
+properties: {
+Place: "Suburban Seattle",
+State: "WA",
+lat: "47.490761",
+lon: "-122.360995"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.360995,
+47.490761
+]
+}
+}
+]
+}
