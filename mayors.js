2c47f67eebe65b78c0bd297e6940b7a236509da5
bellmast <jbm444@gmail.com>
2013-11-14 17:38:39 -0600

    Create mayors.js

---
+++ mayors.js
@@ -0,0 +1,620 @@
+var mayorsConf = {
+type: "FeatureCollection",
+features: [
+{
+type: "Feature",
+properties: {
+lat: "42.6583661",
+lon: "-83.1499322",
+Name: "Bryan Barnett"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-83.1499322,
+42.6583661
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "42.732535",
+lon: "-84.5555347",
+Name: "Virgil P. Bernero"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5555347,
+42.732535
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "44.0234",
+lon: "-92.46295",
+Name: "Ardell Brede"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.46295,
+44.0234
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Harlan Brownlee"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "41.223",
+lon: "-111.9738304",
+Name: "Mike Caldwell"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.97383039999998,
+41.223
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "40.7607793",
+lon: "-111.8910474",
+Name: "Stuart Clason"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.8910474,
+40.7607793
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.5296329",
+lon: "-119.8138027",
+Name: "Andrew Clinger"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.81380270000001,
+39.5296329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Roderick Cox"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "38.6592152",
+lon: "-87.172789",
+Name: "John Dearie"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.172789,
+38.6592152
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.5296329",
+lon: "-119.8138027",
+Name: "Oscar Delgado"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.81380270000001,
+39.5296329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Peter deSilva"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Chris Evans"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "33.7489954",
+lon: "-84.3879824",
+Name: "Duriya Farooqui"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.3879824,
+33.7489954
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "38.2526647",
+lon: "-85.7584557",
+Name: "Greg Fischer"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.7584557,
+38.2526647
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Tami Greenberg"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "41.9778795",
+lon: "-91.6656232",
+Name: "Kris Gulick"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-91.66562319999998,
+41.9778795
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.114053",
+lon: "-94.6274636",
+Name: "Mark Holland"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.6274636,
+39.114053
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Sylvester James, Jr."
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "44.7677424",
+lon: "-93.2777226",
+Name: "Elizabeth Kautz"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.2777226,
+44.7677424
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "33.7489954",
+lon: "-84.3879824",
+Name: "Katie Kirkpatrick"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.3879824,
+33.7489954
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "35.9131996",
+lon: "-79.0558445",
+Name: "Mark Kleinschmidt"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.0558445,
+35.9131996
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "42.732535",
+lon: "-84.5555347",
+Name: "Antonia Kraus"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.5555347,
+42.732535
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "33.7489954",
+lon: "-84.3879824",
+Name: "Bari Love"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.3879824,
+33.7489954
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "46.6715593",
+lon: "-93.0074403",
+Name: "Ann Markusen"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.00744029999998,
+46.6715593
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "28.0344621",
+lon: "-80.5886646",
+Name: "Edyie McCall"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.5886646,
+28.0344621
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "38.9517053",
+lon: "-92.3340724",
+Name: "Robert McDavid"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.3340724,
+38.9517053
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "John McGurk"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Luke Norris"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "38.9108408",
+lon: "-94.3821724",
+Name: "Randy Rhoads"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.3821724,
+38.9108408
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "35.1495343",
+lon: "-90.0489801",
+Name: "Douglas Scarboro"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.0489801,
+35.1495343
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.5296329",
+lon: "-119.8138027",
+Name: "Hillary Schieve"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.81380270000001,
+39.5296329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "33.9204354",
+lon: "-80.3414693",
+Name: "Eric Shytle"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.3414693,
+33.9204354
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "43.0730517",
+lon: "-89.4012302",
+Name: "Paul Soglin"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.4012302,
+43.0730517
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "34.7464809",
+lon: "-92.2895948",
+Name: "Mark Stodola"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-92.2895948,
+34.7464809
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "47.2528768",
+lon: "-122.4442906",
+Name: "Marilyn Strickland"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.44429059999999,
+47.2528768
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Paul Tyler"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "32.8140177",
+lon: "-96.9488945",
+Name: "Beth Van Duyne"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.9488945,
+32.8140177
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.1156615",
+lon: "-77.5636015",
+Name: "Tom Watson"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.5636015,
+39.1156615
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "39.0997265",
+lon: "-94.5785667",
+Name: "Joni Wickham"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "38.4192496",
+lon: "-82.445154",
+Name: "Steve Williams"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.445154,
+38.4192496
+]
+}
+},
+{
+type: "Feature",
+properties: {
+lat: "35.9131996",
+lon: "-79.0558445",
+Name: "Ted Zoller"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.0558445,
+35.9131996
+]
+}
+}
+]
+} 
