a8fea3c80af0ee9e7ebcc77798da43a02e36829f
bellmast <jbm444@gmail.com>
2013-11-19 17:13:57 -0600

    Update 1MC.js

--- 1MC.js
+++ 1MC.js
@@ -1,19 +1,25 @@
 var oMC = {
 type: "FeatureCollection",
+crs: {
+type: "name",
+properties: {
+name: "urn:ogc:def:crs:OGC:1.3:CRS84"
+}
+},
 features: [
 {
 type: "Feature",
 properties: {
-City: "Kansas City",
-State: "MO",
-lat: "39.0997265",
-lon: "-94.5785667"
+City: "Bismarck",
+State: "ND",
+lat: "46.8083268",
+lon: "-100.7837392"
 },
 geometry: {
 type: "Point",
 coordinates: [
--94.5785667,
-39.0997265
+-100.7837392,
+46.8083268
 ]
 }
 },
@@ -28,7 +34,7 @@ lon: "-91.6656232"
 geometry: {
 type: "Point",
 coordinates: [
--91.6656232,
+-91.66562319999998,
 41.9778795
 ]
 }
@@ -84,6 +90,22 @@ coordinates: [
 {
 type: "Feature",
 properties: {
+City: "Dallas",
+State: "TX",
+lat: "32.7801399",
+lon: "-96.8004511"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.8004511,
+32.7801399
+]
+}
+},
+{
+type: "Feature",
+properties: {
 City: "Georgetown",
 State: "DE",
 lat: "38.6901134",
@@ -116,16 +138,32 @@ coordinates: [
 {
 type: "Feature",
 properties: {
-City: "Des Moines",
+City: "West Des Moines, IA",
 State: "IA",
-lat: "41.6005448",
-lon: "-93.6091064"
+lat: "41.5772115",
+lon: "-93.711332"
 },
 geometry: {
 type: "Point",
 coordinates: [
--93.6091064,
-41.6005448
+-93.711332,
+41.5772115
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Fort Worth",
+State: "TX",
+lat: "32.725409",
+lon: "-97.3208496"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.3208496,
+32.725409
 ]
 }
 },
@@ -156,8 +194,24 @@ lon: "-91.5301683"
 geometry: {
 type: "Point",
 coordinates: [
--91.5301683,
+-91.53016829999999,
 41.6611277
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Kansas City",
+State: "MO",
+lat: "39.0997265",
+lon: "-94.5785667"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.5785667,
+39.0997265
 ]
 }
 },
@@ -180,6 +234,38 @@ coordinates: [
 {
 type: "Feature",
 properties: {
+City: "Leesburg",
+State: "VA",
+lat: "39.1156615",
+lon: "-77.5636015"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.5636015,
+39.1156615
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Omaha",
+State: "NE",
+lat: "41.2523634",
+lon: "-95.9979883"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.9979883,
+41.2523634
+]
+}
+},
+{
+type: "Feature",
+properties: {
 City: "Orlando",
 State: "FL",
 lat: "28.5383355",
@@ -196,6 +282,22 @@ coordinates: [
 {
 type: "Feature",
 properties: {
+City: "Peoria",
+State: "IL",
+lat: "40.6936488",
+lon: "-89.5889864"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.5889864,
+40.6936488
+]
+}
+},
+{
+type: "Feature",
+properties: {
 City: "Reno",
 State: "NV",
 lat: "39.5296329",
@@ -204,8 +306,40 @@ lon: "-119.8138027"
 geometry: {
 type: "Point",
 coordinates: [
--119.8138027,
+-119.81380270000001,
 39.5296329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Durham",
+State: "NC",
+lat: "35.9940329",
+lon: "-78.898619"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.898619,
+35.9940329
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "San Diego",
+State: "CA",
+lat: "32.7153292",
+lon: "-117.1572551"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-117.1572551,
+32.7153292
 ]
 }
 },
@@ -220,8 +354,56 @@ lon: "-90.1994042"
 geometry: {
 type: "Point",
 coordinates: [
--90.1994042,
+-90.19940419999999,
 38.6270025
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "St. Petersburg",
+State: "FL",
+lat: "27.7730556",
+lon: "-82.64"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.64,
+27.7730556
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Tulsa",
+State: "OK",
+lat: "36.1539816",
+lon: "-95.992775"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.992775,
+36.1539816
+]
+}
+},
+{
+type: "Feature",
+properties: {
+City: "Wilmington",
+State: "DE",
+lat: "39.7458333",
+lon: "-75.5466667"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.5466667,
+39.7458333
 ]
 }
 }
