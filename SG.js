64691cbfcf253f10bc08cc8cfe0d6df8283f5aad
bellmast <jbm444@gmail.com>
2013-11-20 10:57:41 -0600

    Create SG.js

---
+++ SG.js
@@ -0,0 +1,1406 @@
+var SG = {
+type: "FeatureCollection",
+crs: {
+type: "name",
+properties: {
+name: "urn:ogc:def:crs:OGC:1.3:CRS84"
+}
+},
+features: [
+{
+type: "Feature",
+properties: {
+city: "Huntsville",
+latitude: "34.73037",
+longitude: "-86.5861"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.5861,
+34.73037
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Madison",
+latitude: "34.67667",
+longitude: "-86.7421"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.7421,
+34.67667
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Arizona",
+latitude: "33.38222",
+longitude: "-112.10055"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-112.10055,
+33.38222
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Flagstaff",
+latitude: "35.20135",
+longitude: "-111.63925"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.63925,
+35.20135
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Tucson",
+latitude: "32.22174",
+longitude: "-110.92648"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-110.92648,
+32.22174
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Berkeley",
+latitude: "37.88245",
+longitude: "-122.27992"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.27992,
+37.88245
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Long Beach",
+latitude: "33.76832",
+longitude: "-118.19562"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.19562,
+33.76832
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Los Angeles",
+latitude: "34.25064",
+longitude: "-118.61481"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.61481,
+34.25064
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Oakland",
+latitude: "37.80436",
+longitude: "-122.27111"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.27111,
+37.80436
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Orange",
+latitude: "33.78779",
+longitude: "-117.85311"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-117.85311,
+33.78779
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Riverside",
+latitude: "33.95335",
+longitude: "-117.39616"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-117.39616,
+33.95335
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Sacramento",
+latitude: "38.58157",
+longitude: "-121.4944"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-121.4944,
+38.58157
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "San Francisco",
+latitude: "37.77493",
+longitude: "-122.41942"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.41942,
+37.77493
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "San Jose",
+latitude: "37.33939",
+longitude: "-121.89496"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-121.89496,
+37.33939
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Santa Monica",
+latitude: "34.01945",
+longitude: "-118.49119"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-118.49119,
+34.01945
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Boulder",
+latitude: "40.01499",
+longitude: "-105.27055"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.27055,
+40.01499
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Colorado Springs",
+latitude: "38.83388",
+longitude: "-104.82136"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-104.82136,
+38.83388
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Denver",
+latitude: "39.73757",
+longitude: "-104.98472"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-104.98472,
+39.73757
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Fort Collins",
+latitude: "40.58526",
+longitude: "-105.08442"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.08442,
+40.58526
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Longmont",
+latitude: "40.16721",
+longitude: "-105.10193"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.10193,
+40.16721
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Louisville",
+latitude: "39.97776",
+longitude: "-105.13193"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.13193,
+39.97776
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Miami",
+latitude: "25.78897",
+longitude: "-80.22644"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.22644,
+25.78897
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Atlanta",
+latitude: "33.749",
+longitude: "-84.38798"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.38798,
+33.749
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Honolulu",
+latitude: "21.30694",
+longitude: "-157.85834"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-157.85834,
+21.30694
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Ames",
+latitude: "42.02335",
+longitude: "-93.62562"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.62562,
+42.02335
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Council Bluffs",
+latitude: "41.26194",
+longitude: "-95.86083"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.86083,
+41.26194
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Des Moines",
+latitude: "41.59158",
+longitude: "-93.55317"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.55317,
+41.59158
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Chicago",
+latitude: "41.87811",
+longitude: "-87.6298"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-87.6298,
+41.87811
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Peoria",
+latitude: "40.69365",
+longitude: "-89.58899"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.58899,
+40.69365
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Kansas City",
+latitude: "39.11405",
+longitude: "-94.62746"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.62746,
+39.11405
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Lawrence",
+latitude: "38.97167",
+longitude: "-95.23525"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.23525,
+38.97167
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Covington",
+latitude: "39.08367",
+longitude: "-84.50855"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.50855,
+39.08367
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Lexington",
+latitude: "38.04058",
+longitude: "-84.50372"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.50372,
+38.04058
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Louisville",
+latitude: "38.20457",
+longitude: "-85.77077"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.77077,
+38.20457
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "New Orleans",
+latitude: "29.95107",
+longitude: "-90.07153"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.07153,
+29.95107
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Shreveport",
+latitude: "32.52515",
+longitude: "-93.75018"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.75018,
+32.52515
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Maryland",
+latitude: "39.04575",
+longitude: "-76.64127"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.64127,
+39.04575
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Ann Arbor",
+latitude: "42.28083",
+longitude: "-83.74304"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-83.74304,
+42.28083
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Detroit",
+latitude: "42.33315",
+longitude: "-83.04956"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-83.04956,
+42.33315
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Kalamazoo",
+latitude: "42.29171",
+longitude: "-85.58723"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.58723,
+42.29171
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Traverse City",
+latitude: "44.76306",
+longitude: "-85.62063"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.62063,
+44.76306
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Minneapolis",
+latitude: "44.98333",
+longitude: "-93.26667"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-93.26667,
+44.98333
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Kansas City",
+latitude: "39.09973",
+longitude: "-94.57857"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-94.57857,
+39.09973
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "St. Louis",
+latitude: "38.627",
+longitude: "-90.1994"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-90.1994,
+38.627
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Bozeman",
+latitude: "45.68346",
+longitude: "-111.0505"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.0505,
+45.68346
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Chapel Hill",
+latitude: "35.9132",
+longitude: "-79.05584"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-79.05584,
+35.9132
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Charlotte",
+latitude: "35.22709",
+longitude: "-80.84313"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.84313,
+35.22709
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Durham",
+latitude: "35.99696",
+longitude: "-78.90124"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.90124,
+35.99696
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Raleigh",
+latitude: "35.77959",
+longitude: "-78.63818"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.63818,
+35.77959
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Fargo",
+latitude: "46.87719",
+longitude: "-96.7898"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.7898,
+46.87719
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Lincoln",
+latitude: "40.85822",
+longitude: "-96.73334"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.73334,
+40.85822
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Omaha",
+latitude: "41.25236",
+longitude: "-95.99799"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.99799,
+41.25236
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Princeton",
+latitude: "40.3573",
+longitude: "-74.66722"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-74.66722,
+40.3573
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "New Mexico",
+latitude: "35.18963",
+longitude: "-106.59779"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-106.59779,
+35.18963
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Albuquerque",
+latitude: "35.1107",
+longitude: "-106.60999"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-106.60999,
+35.1107
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Santa Fe",
+latitude: "35.68698",
+longitude: "-105.9378"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-105.9378,
+35.68698
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Reno",
+latitude: "39.54504",
+longitude: "-119.81578"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-119.81578,
+39.54504
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Albany",
+latitude: "42.65258",
+longitude: "-73.75623"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.75623,
+42.65258
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Buffalo",
+latitude: "42.88645",
+longitude: "-78.87837"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-78.87837,
+42.88645
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Hauppauge",
+latitude: "40.82565",
+longitude: "-73.20261"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.20261,
+40.82565
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Ithaca",
+latitude: "42.44396",
+longitude: "-76.50188"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.50188,
+42.44396
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Melville",
+latitude: "40.79343",
+longitude: "-73.41512"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.41512,
+40.79343
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Nesconset",
+latitude: "40.85204",
+longitude: "-73.154"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.154,
+40.85204
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "New York",
+latitude: "40.8233",
+longitude: "-73.94186"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.94186,
+40.8233
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Patchogue",
+latitude: "40.76565",
+longitude: "-73.01511"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.01511,
+40.76565
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Rochester",
+latitude: "43.12627",
+longitude: "-77.62903"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.62903,
+43.12627
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Smithtown",
+latitude: "40.85593",
+longitude: "-73.20067"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.20067,
+40.85593
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Stony Brook",
+latitude: "40.92875",
+longitude: "-73.133"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.133,
+40.92875
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Syracuse",
+latitude: "43.04812",
+longitude: "-76.14742"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.14742,
+43.04812
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Utica",
+latitude: "43.1009",
+longitude: "-75.23266"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.23266,
+43.1009
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Port Jefferson",
+latitude: "40.94649",
+longitude: "-73.06927"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-73.06927,
+40.94649
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Cincinnati",
+latitude: "39.12149",
+longitude: "-84.50911"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-84.50911,
+39.12149
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Cleveland",
+latitude: "41.4995",
+longitude: "-81.69541"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-81.69541,
+41.4995
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Columbus",
+latitude: "39.99057",
+longitude: "-83.04218"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-83.04218,
+39.99057
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Tulsa",
+latitude: "36.15398",
+longitude: "-95.99277"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-95.99277,
+36.15398
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Oregon",
+latitude: "43.80413",
+longitude: "-120.5542"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-120.5542,
+43.80413
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Pittsburgh",
+latitude: "40.44636",
+longitude: "-80.01513"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-80.01513,
+40.44636
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Greenville",
+latitude: "34.85262",
+longitude: "-82.39401"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-82.39401,
+34.85262
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "South Dakota",
+latitude: "43.96951",
+longitude: "-99.90181"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-99.90181,
+43.96951
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Chattanooga",
+latitude: "35.04563",
+longitude: "-85.30968"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-85.30968,
+35.04563
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Memphis",
+latitude: "35.15592",
+longitude: "-89.77743"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-89.77743,
+35.15592
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Nashville",
+latitude: "36.2562",
+longitude: "-86.71435"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-86.71435,
+36.2562
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Austin",
+latitude: "30.32764",
+longitude: "-97.73985"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.73985,
+30.32764
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Dallas",
+latitude: "32.78138",
+longitude: "-96.79692"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-96.79692,
+32.78138
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Fort Worth",
+latitude: "32.75922",
+longitude: "-97.33248"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-97.33248,
+32.75922
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Ogden",
+latitude: "41.223",
+longitude: "-111.97383"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.97383,
+41.223
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Park City",
+latitude: "40.64606",
+longitude: "-111.49797"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.49797,
+40.64606
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Provo",
+latitude: "40.23384",
+longitude: "-111.65853"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.65853,
+40.23384
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Salt Lake City",
+latitude: "40.76078",
+longitude: "-111.89105"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-111.89105,
+40.76078
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Norfolk",
+latitude: "36.85077",
+longitude: "-76.28587"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-76.28587,
+36.85077
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Richmond",
+latitude: "37.55335",
+longitude: "-77.46228"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-77.46228,
+37.55335
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Virginia Beach",
+latitude: "36.85293",
+longitude: "-75.97799"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-75.97799,
+36.85293
+]
+}
+},
+{
+type: "Feature",
+properties: {
+city: "Seattle",
+latitude: "47.60621",
+longitude: "-122.33207"
+},
+geometry: {
+type: "Point",
+coordinates: [
+-122.33207,
+47.60621
+]
+}
+}
+]
+}
